d91fd3a99474028c34cb932628e4dc5b9b589033
Natalia <fishchukova.natalia@gmail.com>
2017-09-06 01:55:58 +0300

    first commit

---
+++ src/main/java/utils/ClickerUtils.java
@@ -0,0 +1,25 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created on 01.09.2017.
+ */
+public class ClickerUtils {
+    private WebDriver driver;
+
+    public ClickerUtils(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void elementClickerWebElement(WebElement wElement) {
+        try {
+            wElement.click();
+//            Thread.sleep( 100 );
+        } catch (WebDriverException e) {
+            e.printStackTrace();
+        }
+    }
+}
